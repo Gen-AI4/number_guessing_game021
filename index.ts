f618a99f20a00469dd3f335eebee35ae3775542e
Ahmed Ali <160950887+Gen-AI4@users.noreply.github.com>
2024-03-23 22:52:22 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+
+const guessNumber = Math.floor (Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([{
+    name: "guessedNumber", type: "number", message: "Guess a number 1-10"
+},
+]);
+
+if (answer.guessedNumber === guessNumber) {
+    console.log("Congratulations! 🎉🎉 You guessed the right number");
+} else {
+    console.log("Sorry! You guessed the wrong number");
+}
